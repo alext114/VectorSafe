667313d5feafc0924c5b46ae887ff46acffa8b8b
alext114 <alext1144@gmail.com>
2017-04-05 15:32:03 -0400

    Add files via upload

---
+++ customerlogin2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>VectorSafe</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="productStyle.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <div class="jumbotron" style="background-color:#CCD1D1;">
+            <div class="container text-center">
+                <h1>VectorSafe</h1>
+                <p>By_Hubbie and the Johnsons</p>
+               
+
+            </div>
+    </div>
+<div style="background-color:#CCD1D1;">
+    <div class="container">
+       <div class="jumbotron" style="background-color:#337ab7;">
+             <p> Welcome to Vector Safe Network Security!<p>
+                <p> Enter your login information to continue</p>
+            <br>
+            <form method="POST" action="authCode.php">
+           <div class="form-group"> <div class="form-group"> <span class="glyphicon glyphicon-lock"></span>
+                 <label for="pwd">Authorize Code:</label>
+                 <input type="number" class="form-control" id="authCode" name="authCode" required>
+           </div>           
+           <button type="submit" name="submit" class="btn btn-primary active">Log In</button>
+           </form>
+           </div>
+       </div>
+       <div class="row">
+</div>
+</div>
+<div class="jumbotron" style="background-color:#CCD1D1;">
+  
+</div>
+<div class="container">
+</div>
+    </body>
+</html>
